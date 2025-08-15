9eeb751dfe310e056404fe9e23d522cd7ac27cb3
prajindra <indrusankar@gmail.com>
2025-08-15 23:01:26 +0800

    deleted postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
-const config = {
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  }
-}
-
-export default config;
+  },
+};
